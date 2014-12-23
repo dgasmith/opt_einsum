c0bbba34dd76bcc469d2c794f6251581ffc69e15
dgasmith <malorian@me.com>
2014-12-23 14:40:39 -0600

    Scope is nearing completion, merged into a single file

--- opt_einsum.py
+++ opt_einsum.py
@@ -2,9 +2,132 @@ import time
 import numpy as np
 import itertools as it
 
-from path_opportunistic import path_opportunistic
-from path_optimal import path_optimal
 
+def _path_optimal(inp, out, ind_dict, memory):
+    inp_set = map(set, inp)
+    out_set = set(out)
+    # Build (total_cost, positions, results, indices_remaining)
+    # positions = [(0,1), (0,2),... etc
+    # results = ['ijkl', 'ckd',... etc
+    # indices_remaining = [set('ijkl'), set('abcd'),... etc
+
+    t = time.time()
+    current = [(0, [], [], inp_set)]
+    for iteration in range(len(inp)-1):
+        new = []
+        # Grab all unique pairs
+        comb_iter = zip(*np.triu_indices(len(inp)-iteration, 1))
+        for curr in current:
+            cost, positions, result, remaining = curr
+            for con in comb_iter:
+
+                # Find contraction indices
+                index_contract = set()
+                index_remain = out_set.copy()
+                new_inp = []
+                for ind, value in enumerate(remaining):
+                    if ind in con:
+                        index_contract |= value
+                    else:
+                        new_inp.append(value)
+                        index_remain |= value
+
+                new_result = index_remain & index_contract
+
+                # Sieve the results based on memory, prevents unnecessarly large tensors
+                out_size = 1
+                for ind in new_result:
+                    out_size *= ind_dict[ind]
+
+                if out_size > memory:
+                    continue
+
+                # Build result
+                index_removed = (index_contract - new_result)
+                new_inp.append(new_result)
+
+                # Find cost
+                new_cost = 1
+                for x in index_contract:
+                    new_cost *= ind_dict[x]
+                new_cost *= 1 + len(index_contract - index_removed)
+
+                # Build new contraction
+                new_cost += cost
+                new_result = result + [new_result]
+                new_pos = positions + [con]
+                new.append((new_cost, new_pos, new_result, new_inp))
+
+        current = new
+
+    new.sort()
+    best = new[0]
+    path = zip(best[1], best[2])
+    einsum_string = ','.join(inp) + '->' + ''.join(out)
+    print 'Path Optimal time: %6d %2d %3.5f %40s' % (len(new), len(inp), (time.time()-t), einsum_string)
+
+    return path
+
+def _compute_size(inds, ind_dict):
+    ret = 1
+    for i in inds:
+        ret *= ind_dict[i]
+    return ret
+
+
+def _path_opportunistic(inp, out, ind_dict, memory):
+    inp_set = map(set, inp)
+    out_set = set(out)
+
+    path = []
+    for iteration in range(len(inp)-1):
+        if len(inp_set)<=1:
+            break
+        iteration_results = []
+        comb_iter = zip(*np.triu_indices(len(inp_set), 1))
+        for positions in comb_iter:
+            index_contract = set()
+            index_remain = out_set.copy()
+            new_inp = []
+            for ind, value in enumerate(inp_set):
+                if ind in positions:
+                    index_contract |= value
+                else:
+                    new_inp.append(value)
+                    index_remain |= value
+
+            # Build index sets
+            index_result = index_remain & index_contract
+            index_removed = (index_contract - index_result)
+
+            # Sieve the results based on memory, prevents unnecessarly large tensors
+            out_size = 1
+            for ind in index_result:
+                out_size *= ind_dict[ind]
+
+            if out_size > memory:
+                continue
+
+            new_inp.append(set(''.join(index_result)))
+
+            # Build sort tuple
+            sum_size = _compute_size(index_contract, ind_dict)
+
+            # Sort tuple
+            removed_size = _compute_size(index_removed, ind_dict)
+            sort = (-removed_size, sum_size)
+            contract = (positions, index_result)
+
+            iteration_results.append([sort, contract, new_inp])
+
+        # Sort based on first index
+        iteration_results.sort()
+        best = iteration_results[0]
+        path.append(best[1])
+
+        inp_set = best[2]
+
+    return path
 
 # Rewrite einsum to handle different cases
 def opt_einsum(string, *views, **kwargs):
@@ -87,9 +210,9 @@ def opt_einsum(string, *views, **kwargs):
     if not isinstance(path_arg, str):
         path = path_arg
     elif path_arg == "opportunistic":
-        path = path_opportunistic(input_list, output_set, dimension_dict, memory_arg)
+        path = _path_opportunistic(input_list, output_set, dimension_dict, memory_arg)
     elif path_arg == "optimal":
-        path = path_optimal(input_list, output_set, dimension_dict, memory_arg)
+        path = _path_optimal(input_list, output_set, dimension_dict, memory_arg)
     else:
         raise KeyError("Path name %s not found", path_arg)
 
