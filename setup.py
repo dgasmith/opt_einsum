259852ff042e94c835ce1257eb662ee8502e596e
stonebig <stonebig34@gmail.com>
2020-02-02 13:16:35 +0100

    add python 3.8
    PyTorch is now released for Python-3.8

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ if __name__ == "__main__":
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
         ],
         zip_safe=True,
         long_description=long_description,
