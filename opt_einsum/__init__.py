feef56ec8154cc75b495c1451d83683ca6010754
Daniel Smith <dgasmith@icloud.com>
2021-05-15 10:21:56 -0400

    Updates YAPF formatting

--- opt_einsum/__init__.py
+++ opt_einsum/__init__.py
@@ -14,6 +14,7 @@ from .path_random import RandomGreedy
 
 # Handle versioneer
 from ._version import get_versions
+
 versions = get_versions()
 __version__ = versions['version']
 __git_revision__ = versions['full-revisionid']
