381f15a78b49c93959f748d3c7cc2e352c195b61
IsaacBreen <57783927+IsaacBreen@users.noreply.github.com>
2022-07-17 14:51:59 +0800

    Revert failing sharing tests for array API backends

--- opt_einsum/tests/test_sharing.py
+++ opt_einsum/tests/test_sharing.py
@@ -6,7 +6,7 @@ import numpy as np
 import pytest
 
 from opt_einsum import contract, contract_expression, contract_path, get_symbol, helpers, shared_intermediates
-from opt_einsum.backends import to_cupy, to_torch, to_array_api, discover_array_api_eps
+from opt_einsum.backends import to_cupy, to_torch
 from opt_einsum.contract import _einsum
 from opt_einsum.parser import parse_einsum_input
 from opt_einsum.sharing import count_cached_ops, currently_sharing, get_sharing_cache
@@ -25,9 +25,7 @@ try:
 except ImportError:
     torch_if_found = pytest.param("torch", marks=[pytest.mark.skip(reason="PyTorch not installed.")])  # type: ignore
 
-array_api_qnames = [ep.value for ep in discover_array_api_eps()]
-
-backends = ["numpy", torch_if_found, cupy_if_found, *array_api_qnames]
+backends = ["numpy", torch_if_found, cupy_if_found]
 equations = [
     "ab,bc->ca",
     "abc,bcd,dea",
@@ -43,7 +41,6 @@ to_backend = {
     "numpy": lambda x: x,
     "torch": to_torch,
     "cupy": to_cupy,
-    **to_array_api,
 }
 
 
@@ -58,7 +55,7 @@ def test_sharing_value(eq, backend):
     with shared_intermediates():
         actual = expr(*views, backend=backend)
 
-    assert np.all(actual == expected)
+    assert (actual == expected).all()
 
 
 @pytest.mark.parametrize("backend", backends)
