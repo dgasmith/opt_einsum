200402e549a9d8a952eb5133cf575ce216318d5a
IsaacBreen <57783927+IsaacBreen@users.noreply.github.com>
2022-07-18 17:38:10 +0800

    Fix formatting and rename some functions for clarity

--- opt_einsum/backends/array_api.py
+++ opt_einsum/backends/array_api.py
@@ -26,42 +26,47 @@ def discover_array_apis():
         return []
 
 
-def make_to_array(array_api: ModuleType) -> Callable:
+def make_to_array_function(array_api: ModuleType) -> Callable:
     """Make a ``to_[array_api]`` function for the given array API."""
+
     @to_backend_cache_wrap
     def to_array(array):  # pragma: no cover
         if isinstance(array, np.ndarray):
             return array_api.asarray(array)
         return array
+
     return to_array
 
-def make_build_expression(array_api: ModuleType) -> Callable:
+
+def make_build_expression_function(array_api: ModuleType) -> Callable:
     """Make a ``build_expression`` function for the given array API."""
+
     def build_expression(_, expr):  # pragma: no cover
         """Build an array API function based on ``arrays`` and ``expr``."""
+
         def array_api_contract(*arrays):
-            return expr._contract([make_to_array(array_api)(x) for x in arrays], backend=array_api.__name__)
+            return expr._contract([make_to_array_function(array_api)(x) for x in arrays], backend=array_api.__name__)
+
         return array_api_contract
+
     return build_expression
 
-def make_evaluate_constants(array_api: ModuleType) -> Callable:
+
+def make_evaluate_constants_function(array_api: ModuleType) -> Callable:
     def evaluate_constants(const_arrays, expr):  # pragma: no cover
         """Convert constant arguments to cupy arrays, and perform any possible constant contractions."""
         return expr(
-            *[make_to_array(array_api)(x) for x in const_arrays],
-            backend=array_api.__name__, 
-            evaluate_constants=True
+            *[make_to_array_function(array_api)(x) for x in const_arrays],
+            backend=array_api.__name__,
+            evaluate_constants=True,
         )
+
     return evaluate_constants
 
-_array_apis = discover_array_apis()
-to_array_api = {api.__name__: make_to_array(api) for api in _array_apis}
-build_expression = {api.__name__: make_build_expression(api) for api in _array_apis}
-evaluate_constants = {api.__name__: make_evaluate_constants(api) for api in _array_apis}
 
-__all__ = [
-    'discover_array_apis',
-    "to_array_api",
-    "build_expression",
-    "evaluate_constants"
-]
+_array_apis = discover_array_apis()
+to_array_api = {api.__name__: make_to_array_function(api) for api in _array_apis}
+build_expression = {api.__name__: make_build_expression_function(api) for api in _array_apis}
+evaluate_constants = {api.__name__: make_evaluate_constants_function(api) for api in _array_apis}
+
+__all__ = ["discover_array_apis", "to_array_api", "build_expression", "evaluate_constants"]
