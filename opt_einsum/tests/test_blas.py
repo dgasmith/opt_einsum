b961fee74e78ac5fd5cd402fdeb9f4b602e93751
Daniel G. A. Smith <dgasmith@icloud.com>
2022-11-02 17:40:04 -0700

    Removes old tensor blas

--- opt_einsum/tests/test_blas.py
+++ opt_einsum/tests/test_blas.py
@@ -5,7 +5,7 @@ Tests the BLAS capability for the opt_einsum module.
 import numpy as np
 import pytest
 
-from opt_einsum import blas, contract, helpers
+from opt_einsum import blas, contract
 
 blas_tests = [
     # DOT
@@ -62,28 +62,6 @@ blas_tests = [
 def test_can_blas(inp, benchmark):
     result = blas.can_blas(*inp)
     assert result == benchmark
-
-
-@pytest.mark.parametrize("inp,benchmark", blas_tests)
-def test_tensor_blas(inp, benchmark):
-
-    # Weed out non-blas cases
-    if benchmark is False:
-        return
-
-    tensor_strs, output, reduced_idx = inp
-    einsum_str = ",".join(tensor_strs) + "->" + output
-
-    # Only binary operations should be here
-    if len(tensor_strs) != 2:
-        assert False
-
-    view_left, view_right = helpers.build_views(einsum_str)
-
-    einsum_result = np.einsum(einsum_str, view_left, view_right)
-    blas_result = blas.tensor_blas(view_left, tensor_strs[0], view_right, tensor_strs[1], output, reduced_idx)
-
-    assert np.allclose(einsum_result, blas_result)
 
 
 def test_blas_out():
