a7356c2ba134ee4add2d12bf6d7606fec812ee43
Daniel Smith <dgasmith@icloud.com>
2026-03-14 19:53:34 -0400

    Removes tensorflow from tests for now

--- opt_einsum/_version.py
+++ opt_einsum/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '3.4.1.dev15+g5e5c49914.d20260314'
-__version_tuple__ = version_tuple = (3, 4, 1, 'dev15', 'g5e5c49914.d20260314')
+__version__ = version = '3.4.1.dev20+gfc6c4ce5e.d20260314'
+__version_tuple__ = version_tuple = (3, 4, 1, 'dev20', 'gfc6c4ce5e.d20260314')
 
 __commit_id__ = commit_id = None
